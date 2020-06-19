0622f46aa9c6cf5887ebd3b4b56e889823c83029
Zach Polsky <zachp1122@gmail.com>
2020-06-19 15:13:46 -0400

    Test basic query param Add console log to app start Make npm start use hot reloads

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Response {
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query('name') name: string): Response {
+    return {
+      message: name ? `Hello, ${name}!` : this.appService.getHello()
+    };
   }
 }
